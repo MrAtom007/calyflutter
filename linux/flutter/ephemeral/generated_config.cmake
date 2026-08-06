bfe4ff91988e88b8b2424f7b74ff8567c6a80c7e
mratom <mratom@users.noreply.github.com>
2026-08-06 14:08:16 +0200

    fix: migra a onReorderItem in health_screen e sistema smoke test

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/mratom/development/sdk/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/mratom/development/git/calyflutter" PROJECT_DIR)
+
+set(FLUTTER_VERSION "4.13.0+32" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 4 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 13 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 32 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/mratom/development/sdk/flutter"
+  "PROJECT_DIR=/home/mratom/development/git/calyflutter"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuNDQuOA==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049MDU4ZTBhZjJjMg==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049MGNkNjEwNzE3Yg==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMi4y"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=true"
+  "PACKAGE_CONFIG=/home/mratom/development/git/calyflutter/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=lib/main.dart"
+)
